b7f84420d376e5ac3cc6b015c6594db66bb2f6e4
Stephen Wells <stephen.wells@guardian.co.uk>
2016-08-16 16:51:23 +0100

    add remote s3 backed configuration.
    This will be used for private urls, passwords etc. the google service account details for analytics login will be stored here.

--- app/services/Config.scala
+++ app/services/Config.scala
@@ -1,5 +1,10 @@
 package services
 
+import java.util.Properties
+import com.amazonaws.services.s3.model.GetObjectRequest
+import services.Config._
+import scala.collection.JavaConversions._
+
 object Config extends AwsInstanceTags {
 
   lazy val conf = readTag("Stage") match {
@@ -22,6 +27,44 @@ sealed trait Config {
   def campaignTableName = s"campaign-central-$stage-campaigns"
   def campaignNotesTableName = s"campaign-central-$stage-campaign-notes"
   def campaignContentTableName = s"campaign-central-$stage-campaign-content"
+
+  // remote configuration is used for things we don't want to check in to version control
+  // such as passwords, private urls, and gossip about other teams
+  private val remoteConfiguration: Map[String, String] = loadRemoteConfiguration
+
+
+
+  private def getRequiredRemoteStringProperty(key: String): String = {
+    remoteConfiguration.getOrElse(key, {
+      throw new IllegalArgumentException(s"Property '$key' not configured")
+    })
+  }
+
+  private def loadRemoteConfiguration = {
+
+    val stack = readTag("Stack") getOrElse "flexible"
+    val stage = readTag("Stage") getOrElse "DEV"
+    val app = readTag("App") getOrElse "tag-manager"
+
+    val bucketName = s"guconf-${stack}"
+
+    def loadPropertiesFromS3(propertiesKey: String, props: Properties): Unit = {
+      val s3Properties = AWS.S3Client.getObject(new GetObjectRequest(bucketName, propertiesKey))
+      val propertyInputStream = s3Properties.getObjectContent
+      try {
+        props.load(propertyInputStream)
+      } finally {
+        try {propertyInputStream.close()} catch {case _: Throwable => /*ignore*/}
+      }
+    }
+
+    val props = new Properties()
+
+    loadPropertiesFromS3(s"$app/global.properties", props)
+    loadPropertiesFromS3(s"$app/$stage.properties", props)
+
+    props.toMap
+  }
 }
 
 class DevConfig extends Config {
