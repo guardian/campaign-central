bc017ed8ea1de21442c2db03b9a6a4564567506c
Zofia <zofia.korcz@guardian.co.uk>
2016-11-24 17:16:15 +0000

    it kinda works

--- public/components/CampaignList/CampaignList.js
+++ public/components/CampaignList/CampaignList.js
@@ -11,6 +11,24 @@ class CampaignList extends React.Component {
     campaigns: []
   };
 
+  sortBy = (field, reverse, iteratees) => {
+    let key = function(x) {return iteratees ? iteratees(x[field]) : x[field]};
+
+    reverse = !reverse ? 1 : -1;
+
+    return function (a, b) {
+      return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+    }
+  }
+
+  invokeSort = (column) => {
+    console.log(this.props.campaigns);
+    this.props.campaigns.sort(this.sortBy(column, false, function(value){
+        return typeof value === "string" ? value.toUpperCase() : value
+      }
+    ));
+  }
+
   render () {
     if (!this.props.campaigns.length) {
       return (
@@ -24,12 +42,12 @@ class CampaignList extends React.Component {
       <table className="campaign-list">
         <thead>
           <tr>
-            <th className="campaign-list__header">Name</th>
-            <th className="campaign-list__header">Type</th>
-            <th className="campaign-list__header">Status</th>
-            <th className="campaign-list__header">Value</th>
-            <th className="campaign-list__header">Start date</th>
-            <th className="campaign-list__header">Finish date</th>
+            <th onClick={this.invokeSort('name')} className="campaign-list__header">Name</th>
+            <th onClick={this.invokeSort('type')} className="campaign-list__header">Type</th>
+            <th onClick={this.invokeSort('status')} className="campaign-list__header">Status</th>
+            <th onClick={this.invokeSort('actualValue')} className="campaign-list__header">Value</th>
+            <th onClick={this.invokeSort('startDate')} className="campaign-list__header">Start date</th>
+            <th onClick={this.invokeSort('endDate')} className="campaign-list__header">Finish date</th>
             <th className="campaign-list__header">Uniques</th>
           </tr>
         </thead>
