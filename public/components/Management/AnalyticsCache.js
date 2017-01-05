90f37c695ddb21a242e5ad1187a398a8a8762c63
Stephen Wells <stephen.wells@guardian.co.uk>
2017-01-05 15:40:31 +0000

    remove deprecated report from cache management interface
    and add new reports

--- public/components/Management/AnalyticsCache.js
+++ public/components/Management/AnalyticsCache.js
@@ -98,7 +98,8 @@ class AnalyticsCache extends Component {
         {this.renderCacheItems()}
         <h3 className="campaigns__header">Refresh all stale items of type:</h3>
         <ul>
-          <li onClick={() => refreshDataType('CampaignDailyCountsReport')}><i className="i-refresh-black" />CampaignDailyCountsReport</li>
+          <li onClick={() => refreshDataType('CampaignPageViewsReport')}><i className="i-refresh-black" />CampaignPageViewsReport</li>
+          <li onClick={() => refreshDataType('DailyUniqueUsersReport')}><i className="i-refresh-black" />DailyUniqueUsersReport</li>
           <li onClick={() => refreshDataType('CtaClicksReport')}><i className="i-refresh-black" />CtaClicksReport</li>
           <li onClick={() => refreshDataType('TrafficDriverGroupStats')}><i className="i-refresh-black" />TrafficDriverGroupStats</li>
         </ul>
