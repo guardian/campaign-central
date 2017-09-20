8ae473349d16704444598db21ef7679281d1123a
Francis Carr <francis.carr@guardian.co.uk>
2017-09-20 16:22:53 +0100

    Add Glossary module

---
+++ public/components/Glossary/Glossary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MedianAttentionTimeExplainerText = 'Attention time is the value where a user is knowingly active on the page; they have recently moved their mouse or scrolled.';
+const DwellTimeExplainerText = 'Dwell time tries to emulate the same viewability metric from Google Analytics. It is calculated by taking the difference between a pageview timestamp and the subsequent pageview timestamp. These pageviews have to happen within a 30 minute session period, and are averaged when there has been more than one pageview for a unique user.';
+
+class Glossary extends React.Component {
+
+  render() {
+    return (
+      <div className="glossary">
+        <h1>Median Attention Time</h1>
+        <p>{MedianAttentionTimeExplainerText}</p>
+        <h1>Average Dwell Time</h1>
+        <p>{DwellTimeExplainerText}</p>
+      </div>
+    );
+  }
+}
+
+export { Glossary, DwellTimeExplainerText, MedianAttentionTimeExplainerText };
