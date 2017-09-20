c32729684096060e79a102c8555f051473279d58
Francis Carr <francis.carr@guardian.co.uk>
2017-09-20 16:49:52 +0100

    Add recent time to text

--- public/components/Glossary/Glossary.js
+++ public/components/Glossary/Glossary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const MedianAttentionTimeExplainerText = 'Attention time is the measure of how long a user is knowingly active on the page; they have recently moved their mouse or scrolled.';
+const MedianAttentionTimeExplainerText = 'Attention time is the measure of how long a user is knowingly active on the page; they have recently moved their mouse or scrolled within 5 seconds, or are watching a video in a focused tab.';
 const DwellTimeExplainerText = 'Dwell time tries to emulate the same viewability metric from Google Analytics. It is calculated by taking the difference between a pageview timestamp and the subsequent pageview timestamp. These pageviews have to happen within a 30 minute session period, and are averaged when there has been more than one pageview for a unique user.';
 
 class Glossary extends React.Component {
