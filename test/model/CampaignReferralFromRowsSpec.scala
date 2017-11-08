bf23127d36982e9ca02fce7de381593175dae3c8
Kelvin Chappell <kelvin.chappell@theguardian.com>
2017-11-08 15:31:34 +0000

    Refactor

--- test/model/CampaignReferralFromRowsSpec.scala
+++ test/model/CampaignReferralFromRowsSpec.scala
@@ -52,12 +52,12 @@ object CampaignReferralFromRowsSpec extends Properties("CampaignReferral.fromRow
 
   property("Stats at any arbitrary level sum to stats of parent") = forAll { rows: Seq[CampaignReferralRow] =>
     val statsMatch = for {
-      gp       <- CampaignReferral.fromRows(rows).headOption
-      p        <- gp.children.flatMap(_.headOption)
-      children <- p.children
+      grandparent <- CampaignReferral.fromRows(rows).headOption
+      parent      <- grandparent.children.flatMap(_.headOption)
+      children    <- parent.children
     } yield {
       val childrenStats = children.map(_.stats)
-      p.stats == ReferralStats(
+      parent.stats == ReferralStats(
         impressionCount = childrenStats.map(_.impressionCount).sum,
         clickCount = childrenStats.map(_.clickCount).sum
       )
