73912b7253c259f8b370024de3e84220c79a83ac
Kelvin Chappell <kelvin.chappell@theguardian.com>
2017-11-14 15:05:46 +0000

    Show explanation for CTRs of > 100%

--- public/components/CampaignAnalytics/Analytics/CampaignReferral.js
+++ public/components/CampaignAnalytics/Analytics/CampaignReferral.js
@@ -4,6 +4,19 @@ import "react-infinity-menu/src/infinity-menu.css";
 
 export default class CampaignReferral extends React.Component {
 
+  explainImpossibleCTR(ctr) {
+    if (ctr > 1) {
+      const explanation = "The behaviour of the back button in the Safari browser is anomalous;"+
+        " following a link from a front, then using the back button" +
+        " to go back to the front, then following the same link again" +
+        " is being counted as a single impression and two clicks rather than two impressions and two clicks." +
+        " This should have a negligible effect except where the number of impressions is very low," +
+        " as in this case.";
+      return <span className="glyphicon glyphicon-warning-sign" title={explanation}></span>
+    }
+    return "";
+  };
+
   render() {
     const depth = Math.floor(this.props.data.keyPath.split(".").length / 2);
     return (
@@ -17,7 +30,8 @@ export default class CampaignReferral extends React.Component {
           <NumberFormat value={this.props.data.clickCount} displayType={'text'} thousandSeparator={true}/>
         </div>
         <div className="pure-u-2-24 campaign-referral-list__ctr">
-          <NumberFormat value={this.props.data.ctr * 100} displayType={'text'} decimalPrecision={2}/>
+          <NumberFormat value={this.props.data.ctr * 100} displayType={'text'} decimalPrecision={2}/>&nbsp;
+          {this.explainImpossibleCTR(this.props.data.ctr)}
         </div>
       </li>
     );
