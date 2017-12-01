b13e8bd64068a8ffcfc59dbc1f2a64f2a980d0d0
Luke Taylor <luke.taylor@guardian.co.uk>
2017-12-01 16:12:49 +0000

    Provide logical groupings for device breakdown.

--- app/model/LatestCampaignAnalytics.scala
+++ app/model/LatestCampaignAnalytics.scala
@@ -5,29 +5,12 @@ import play.api.libs.json.Format
 
 import scala.collection.immutable.ListMap
 
-case class LatestAnalyticsBreakdownItem(uniques: Long,
-                                        pageviews: Long,
-                                        timeSpentOnPage: Option[Double] = None,
-                                        facebookShares: Option[Long] = None,
-                                        linkedInShares: Option[Long] = None)
-
-object LatestAnalyticsBreakdownItem {
-  implicit val latestCampaignAnalyticsBreakdownItemFormat: Format[LatestAnalyticsBreakdownItem] =
-    Jsonx.formatCaseClass[LatestAnalyticsBreakdownItem]
-}
-
 case class LatestCampaignAnalytics(campaignId: String,
                                    uniques: Long,
-                                   uniquesByDevice: Option[Map[String, Long]],
                                    uniquesTarget: Long,
                                    pageviews: Long,
-                                   pageviewsByDevice: Option[Map[String, Long]],
                                    medianAttentionTimeSeconds: Option[Long],
-                                   medianAttentionTimeByDevice: Option[Map[String, Long]],
                                    weightedAverageDwellTimeForCampaign: Option[Double],
-                                   averageDwellTimePerPathSeconds: Option[Map[String, Double]],
-                                   averageDwellTimePerDeviceSeconds: Option[Map[String, Double]],
-                                   averageDwellTimePerCountryCodeSeconds: Option[Map[String, Double]],
                                    analyticsByCountryCode: Map[String, LatestAnalyticsBreakdownItem],
                                    analyticsByDevice: Map[String, LatestAnalyticsBreakdownItem],
                                    analyticsByPath: Map[String, LatestAnalyticsBreakdownItem])
@@ -36,34 +19,41 @@ object LatestCampaignAnalytics {
   implicit val latestCampaignAnalyticsFormat: Format[LatestCampaignAnalytics] =
     Jsonx.formatCaseClass[LatestCampaignAnalytics]
 
+  private val LogicalDeviceGroupings: Map[String, List[String]] = Map(
+    "Desktop" -> List("PERSONAL_COMPUTER"),
+    "Tablet"  -> List("TABLET"),
+    "Mobile"  -> List("SMARTPHONE"),
+    "App"     -> List("GUARDIAN_IOS_NATIVE_APP", "GUARDIAN_ANDROID_NATIVE_APP"),
+    "Other"   -> List("GAME_CONSOLE", "OTHER", "UNKNOWN")
+  )
+
   def apply(latestCampaignAnalyticsItem: LatestCampaignAnalyticsItem, uniquesTarget: Long): LatestCampaignAnalytics = {
 
     import util.DoubleUtils._
-
-    def sortByUniques: ((String, LatestAnalyticsBreakdownItem), (String, LatestAnalyticsBreakdownItem)) => Boolean = {
-      case ((_, lb1), (_, lb2)) => lb1.uniques > lb2.uniques
-    }
-
-    val metricsByCountryCode: Map[String, LatestAnalyticsBreakdownItem] =
-      latestCampaignAnalyticsItem.uniquesByCountryCode
-        .map { uniquesByCountryCode =>
-          uniquesByCountryCode.flatMap {
-            case (key, uniques) =>
-              val maybePageviews = latestCampaignAnalyticsItem.pageviewsByCountryCode.flatMap(_.get(key))
-              val maybeTimeSpentOnPage =
-                latestCampaignAnalyticsItem.averageDwellTimePerCountryCodeSeconds.flatMap(_.get(key))
-              maybePageviews.map { pageviews =>
-                key -> LatestAnalyticsBreakdownItem(uniques, pageviews, maybeTimeSpentOnPage)
-              }
-          }
-        }
-        .getOrElse(Map.empty)
+    import cats.implicits._
+    import model.LatestAnalyticsBreakdownItem._
 
     val top20PlusSumOfOtherMetricsByCountryCode: Map[String, LatestAnalyticsBreakdownItem] = {
+
+      val metricsByCountryCode: Map[String, LatestAnalyticsBreakdownItem] =
+        latestCampaignAnalyticsItem.uniquesByCountryCode
+          .map { uniquesByCountryCode =>
+            uniquesByCountryCode.flatMap {
+              case (key, uniques) =>
+                val maybePageviews = latestCampaignAnalyticsItem.pageviewsByCountryCode.flatMap(_.get(key))
+                val maybeTimeSpentOnPage =
+                  latestCampaignAnalyticsItem.averageDwellTimePerCountryCodeSeconds.flatMap(_.get(key))
+                maybePageviews.map { pageviews =>
+                  key -> LatestAnalyticsBreakdownItem(uniques, pageviews, maybeTimeSpentOnPage)
+                }
+            }
+          }
+          .getOrElse(Map.empty)
+
       val sortedMetricsByCountryCode = ListMap(metricsByCountryCode.toSeq.sortWith(sortByUniques): _*)
       val (top20, other)             = sortedMetricsByCountryCode.splitAt(20)
 
-      top20 + ("other" ->
+      top20 + ("Other" ->
         LatestAnalyticsBreakdownItem(
           uniques = other.values.map(_.uniques).sum,
           pageviews = other.values.map(_.pageviews).sum,
@@ -71,15 +61,22 @@ object LatestCampaignAnalytics {
         ))
     }
 
-    val metricsByDevice: Map[String, LatestAnalyticsBreakdownItem] =
-      latestCampaignAnalyticsItem.uniquesByDevice.getOrElse(Map.empty).flatMap {
-        case (key, uniques) =>
-          val maybePageviews       = latestCampaignAnalyticsItem.pageviewsByDevice.flatMap(_.get(key))
-          val maybeTimeSpentOnPage = latestCampaignAnalyticsItem.averageDwellTimePerDeviceSeconds.flatMap(_.get(key))
-          maybePageviews.map { pageviews =>
-            key -> LatestAnalyticsBreakdownItem(uniques, pageviews, maybeTimeSpentOnPage)
-          }
+    val metricsByDevice: Map[String, LatestAnalyticsBreakdownItem] = {
+      LogicalDeviceGroupings.map {
+        case (label, grouping) =>
+          val breakdownSummary: LatestAnalyticsBreakdownItem = grouping.flatMap { key =>
+            for {
+              uniques   <- latestCampaignAnalyticsItem.uniquesByDevice.flatMap(_.get(key))
+              pageviews <- latestCampaignAnalyticsItem.pageviewsByDevice.flatMap(_.get(key))
+              maybeTimeSpentOnPage = latestCampaignAnalyticsItem.averageDwellTimePerDeviceSeconds.flatMap(_.get(key))
+            } yield {
+              LatestAnalyticsBreakdownItem(uniques, pageviews, maybeTimeSpentOnPage)
+            }
+          }.combineAll
+
+          label -> breakdownSummary
       }
+    }
 
     val metricsByPath: Map[String, LatestAnalyticsBreakdownItem] =
       latestCampaignAnalyticsItem.averageDwellTimePerPathSeconds.getOrElse(Map.empty).map {
@@ -93,32 +90,14 @@ object LatestCampaignAnalytics {
     LatestCampaignAnalytics(
       latestCampaignAnalyticsItem.campaignId,
       latestCampaignAnalyticsItem.uniques,
-      latestCampaignAnalyticsItem.uniquesByDevice.map(normaliseDeviceData),
       uniquesTarget,
       latestCampaignAnalyticsItem.pageviews,
-      latestCampaignAnalyticsItem.pageviewsByDevice.map(normaliseDeviceData),
       latestCampaignAnalyticsItem.medianAttentionTimeSeconds,
-      latestCampaignAnalyticsItem.medianAttentionTimeByDevice.map(normaliseDeviceData),
       latestCampaignAnalyticsItem.weightedAverageDwellTimeForCampaign.map(_.to2Dp),
-      latestCampaignAnalyticsItem.averageDwellTimePerPathSeconds.map(_.mapValues(_.to2Dp)),
-      latestCampaignAnalyticsItem.averageDwellTimePerDeviceSeconds.map(_.mapValues(_.to2Dp)),
-      latestCampaignAnalyticsItem.averageDwellTimePerCountryCodeSeconds.map(_.mapValues(_.to2Dp)),
       top20PlusSumOfOtherMetricsByCountryCode,
       ListMap(metricsByDevice.toSeq.sortWith(sortByUniques): _*),
       ListMap(metricsByPath.toSeq.sortWith(sortByUniques): _*)
     )
   }
 
-  private def normaliseDeviceData(data: Map[String, Long]): Map[String, Long] = {
-    data.map {
-      case (deviceType, medianAttentionTime) =>
-        deviceType match {
-          case "GUARDIAN_IOS_NATIVE_APP"     => "IOS APP"     -> medianAttentionTime
-          case "GUARDIAN_ANDROID_NATIVE_APP" => "ANDROID APP" -> medianAttentionTime
-          case "PERSONAL_COMPUTER"           => "DESKTOP"     -> medianAttentionTime
-          case "SMARTPHONE"                  => "MOBILE"      -> medianAttentionTime
-          case other                         => other         -> medianAttentionTime
-        }
-    }
-  }
 }
