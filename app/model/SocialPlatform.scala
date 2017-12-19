ca9fea7df6ba8f2e4177575432d66bcbf323dab9
Kelvin Chappell <kelvin.chappell@theguardian.com>
2017-12-19 14:44:09 +0000

    Support more social platforms for referrals

--- app/model/SocialPlatform.scala
+++ app/model/SocialPlatform.scala
@@ -12,9 +12,34 @@ case object Facebook extends SocialPlatform {
   val name: String   = "Facebook"
 }
 
+case object Instagram extends SocialPlatform {
+  val domain: String = "instagram.com"
+  val name: String   = "Instagram"
+}
+
+case object LinkedIn extends SocialPlatform {
+  val domain: String = "linkedin.com"
+  val name: String   = "Linked In"
+}
+
+case object Pocket extends SocialPlatform {
+  val domain: String = "getpocket.com"
+  val name: String   = "Pocket"
+}
+
+case object Reddit extends SocialPlatform {
+  val domain: String = "reddit.com"
+  val name: String   = "Reddit"
+}
+
 case object Twitter extends SocialPlatform {
   val domain: String = "twitter.com"
   val name: String   = "Twitter"
+}
+
+case object Youtube extends SocialPlatform {
+  val domain: String = "youtube.com"
+  val name: String   = "Youtube"
 }
 
 case object Unknown extends SocialPlatform {
@@ -26,8 +51,13 @@ object SocialPlatform {
   implicit lazy val writes: Writes[SocialPlatform] = platform => JsString(platform.name)
 
   def fromDomain(domain: String): SocialPlatform = domain match {
-    case Facebook.domain => Facebook
-    case Twitter.domain  => Twitter
-    case _               => Unknown
+    case Facebook.domain  => Facebook
+    case Instagram.domain => Instagram
+    case LinkedIn.domain  => LinkedIn
+    case Pocket.domain    => Pocket
+    case Reddit.domain    => Reddit
+    case Twitter.domain   => Twitter
+    case Youtube.domain   => Youtube
+    case _                => Unknown
   }
 }
